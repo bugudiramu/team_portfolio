4e122f72f9471e32203b50e91302c557bdce09c3
Ramu <37015092+bugudiramu@users.noreply.github.com>
2020-11-27 13:44:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.state = {
+      isScrolling: false,
+      clientX: 0,
+      scrollX: 0,
+    };
+  }
+
+  onMouseDown = (e) => {
+    this.setState({ ...this.state, isScrolling: true, clientX: e.clientX });
+  };
+
+  onMouseUp = () => {
+    this.setState({ ...this.state, isScrolling: false });
+  };
+
+  onMouseMove = (e) => {
+    const { clientX, scrollX } = this.state;
+    if (this.state.isScrolling) {
+      this.ref.current.scrollLeft = scrollX + e.clientX - clientX;
+      this.state.scrollX = scrollX + e.clientX - clientX;
+      this.state.clientX = e.clientX;
+    }
+  };
+
+  render() {
+    const { ref, rootClass } = this.props;
+    return (
+      <div
+        ref={ref}
+        onMouseDown={this.onMouseDown}
+        onMouseUp={this.onMouseUp}
+        onMouseMove={this.onMouseMove}
+        className={rootClass}
+      >
+        {React.Children.map(this.props.children, (child) =>
+          React.Children.only(child)
+        )}
+      </div>
+    );
+  }
 }
+
+App.defaultProps = {
+  ref: { current: {} },
+  rootClass: '',
+};
+
+App.propTypes = {
+  ref: PropTypes.object,
+  rootClass: PropTypes.string,
+  children: PropTypes.string,
+};
 
 export default App;
