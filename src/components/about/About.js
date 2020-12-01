cb353380fa7484e6113bf6d0460f14b3e58fdae2
Kanike Kalyan Kumar <37512504+kkkumarkanike@users.noreply.github.com>
2020-12-01 14:56:36 +0530

    about started

---
+++ src/components/about/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AboutStyles.js';
+import AboutItem from './AboutItem';
+
+const About = () => {
+     return (
+          <div></div>
+     );
+}
+
+export default About;
---
+++ src/components/about/AboutItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AboutStyles.js';
+
+const AboutItem = () => {
+     return (
+          <div></div>
+     );
+}
+
+export default AboutItem;
